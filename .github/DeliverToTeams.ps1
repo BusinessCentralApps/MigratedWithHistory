804e9316b1fd40a7f4980c49378e58403f805f20
freddydk <freddyk@microsoft.com>
2023-01-19 07:28:57 +0100

    apply template

--- .github/DeliverToTeams.ps1
+++
@@ -1,9 +0,0 @@
-Param(
-    [Hashtable]$parameters
-)
-
-Write-Host $parameters.project
-Write-Host $parameters.projectName
-$parameters.RepoSettings | Out-Host
-$parameters.ProjectSettings | Out-Host
-$parameters.Context.ToUpper() | Out-Host
